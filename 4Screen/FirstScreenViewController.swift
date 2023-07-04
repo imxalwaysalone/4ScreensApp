97730e593297f656f83a6b3691cc29c7bdfb3579
imxalwaysalone <127434683+imxalwaysalone@users.noreply.github.com>
2023-07-04 21:19:41 +0300

    create first screen
    add navigation and base ui elements

---
+++ 4Screen/FirstScreenViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FirstScreenViewController.swift
+//  4Screen
+//
+//  Created by Владимир Дроздов on 4.07.23.
+//
+
+import UIKit
+
+class FirstScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
